631f62db5aacabd1a2653c3b9d9c14d83a9a2e27
laibazahoor388 <laibazahoor388@gmail.com>
2026-05-11 11:38:47 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Laiba Zahoor Portfolio Loaded Successfully 🚀");
+
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Welcome to Laiba Zahoor Portfolio Website!");
+});
